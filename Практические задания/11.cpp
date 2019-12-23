b865b72915bf2bd15cf24b83cae8857a68e6b457
DaniilKelnik <55651938+DaniilKelnik@users.noreply.github.com>
2019-12-23 22:10:43 +0300

    Add files via upload

---
+++ Практические задания/11.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "math.h"
+using namespace std;
+int main()
+{
+	int a, b, n;
+	cin >> a;
+	if (a > 0) 
+	{
+		b = 1;
+		for (n = 1;a>= n; n++)
+		{
+			b = b * n;
+		}
+		cout << b << endl;
+	}
+	else if (a==0)
+	{
+		b = 1;
+		cout << "b=" << b << endl;
+	}
+	else
+	{
+		cout << "Число не может быть ,меньше 0" << endl;
+	}
+	system("Pause");
+	return 0;
+}
